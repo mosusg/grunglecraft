41871ab10e0878c6304b3868091bcab0967ace59
Linkmain <moses.garado@gmail.com>
2024-05-18 13:51:21 -0700

    Grungle lives

---
+++ src/main/java/net/mosus/grunglecraft/GrunglecraftClient.java
@@ -0,0 +1,10 @@
+package net.mosus.grunglecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GrunglecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mosus/grunglecraft/GrunglecraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mosus.grunglecraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GrunglecraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
